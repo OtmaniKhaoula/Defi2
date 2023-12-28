8029323d97454ed411879931de7fcc55f246f60f
ELISA_MARTIN <115727083+ELISA-MARTIN@users.noreply.github.com>
2023-12-28 05:49:36 +0100

    Update dict_word.py

--- tools/dict_word.py
+++ tools/dict_word.py
@@ -38,21 +38,14 @@ with open("../polarity_word.txt", encoding = "latin-1") as fichier:
         # Retirer les éventuels espaces autour des parties
         ligne_tokenized = [l.strip() for l in ligne]
         ligne_tokenized[1] = ligne_tokenized[1].replace('"', '')
-        if(len(ligne_tokenized[1].split(" "))>1 or len(ligne_tokenized[1].split(">"))>1):
+        if(bool(re.search(r"\d", ligne_tokenized[1])) or not bool(re.search('[a-zA-Z]', ligne_tokenized[1])) or len(ligne_tokenized[1].split(" "))>1 or len(ligne_tokenized[1].split(">"))>1):
             continue
-        #print(ligne_tokenized)
-        #ligne_tokenized = ligne.split(";")
         if(ligne_tokenized[2] == ligne_tokenized[4]):
             dict_emotion[ligne_tokenized[1]] = "neutre"
             continue
         A = [float(element) for element in ligne_tokenized[2:]]
         idx = np.argmax(A, axis=0)
-        #print("word = ", ligne_tokenized[1])
-        #print("liste = ", A)
-        #print("idx = ", idx)
-        #"&#273;&#432;&#7901;i &#432;&#417;i"
         dict_emotion[ligne_tokenized[1]] = dict[idx]
-        #print("tokenized = ", ligne_tokenized[1])
 
 print("taille = ", len(dict_emotion))
 np.save(f"{path}/processed_data/dict_polarity.npy", dict_emotion)
@@ -60,25 +53,23 @@ np.save(f"{path}/processed_data/dict_polarity.npy", dict_emotion)
 df = pd.DataFrame(list(dict_emotion.items()))
 df.to_csv(f"{path}/processed_data/dict_polarity.csv", encoding = "latin-1", header = False)
 
-#dict_emotion = {"film": "neutre"}
-print("film = ", dict_emotion["film"])
-
 # Ajouter les polarités dans les commentaires
 cpt = 0
 list_keyword = list(dict_emotion.keys())
+
 for review in comments.keys():
-    if (isinstance(comments[review], float)):
+    if isinstance(comments[review], float):
         continue
+
     review_split = comments[review].split(" ")
-    for word, i in zip(review_split, [i for i in range(len(review_split))]):
-        if (word in list_keyword):
-            review_split[i] = word + " " + dict_emotion[word]
-            #print("review_split = ", review_split[i])
+    
+    modified_review = [word + " " + dict_emotion.get(word, "") if word in list_keyword else word for word in review_split]
+    
+    comments[review] = " ".join(modified_review)
+    print(comments[review])
+    
     cpt += 1
-    #print("cpt = ", cpt)
     if cpt % 100 == 0:
-        print("cpt = ", cpt)
-    comments[review] = " ".join(review_split)
-    #print("modified_words = ", comments_train[review])
+        print("cpt =", cpt)
     
 np.save(f"{path}/processed_data/{file}/comments_with_polarities.npy", comments)
