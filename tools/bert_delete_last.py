e1eaea595c5e75d069e8084674cb20d46c362d26
ELISA_MARTIN <115727083+ELISA-MARTIN@users.noreply.github.com>
2023-12-28 05:46:36 +0100

    Update bert_delete_last.py
    Dernière version bert_delete_last.py

--- tools/bert_delete_last.py
+++ tools/bert_delete_last.py
@@ -2,34 +2,27 @@
 """
 Created on Thu Nov 16 16:01:25 2023
 
-@author: Elisa Martin
+@author: Elisa Martin & Khaoula Otmani
 
 Lien: https://ledatascientist.com/analyse-de-sentiments-avec-camembert/
 """
-
 """
 Importation de librairies
 """
 
 import torch
-import seaborn
 import pandas as pd
 import numpy as np
-from sklearn import metrics
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from transformers import CamembertForSequenceClassification, CamembertTokenizer, AdamW
 import sys, os
 import torch.nn as nn
-import torch.nn.functional as F
-from sklearn.metrics import classification_report
-import transformers
-from transformers import BertForSequenceClassification, AdamW, BertConfig
 
 directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(directory)
 
-import configg
-path = configg.paths['url']
+import config
+path = config.paths['url']
 
 if torch.cuda.is_available():
     print("GPU disponible!")
@@ -122,6 +115,7 @@ Encodage du texte
 # cmarkea/distilcamembert-base-sentiment
 # 'do_lower_case' à True pour qu'on passe tout en miniscule
 text = 'cmarkea/distilcamembert-base-sentiment'
+#text = 'tblard/tf-allocine'
 TOKENIZER = CamembertTokenizer.from_pretrained(
     text,
     do_lower_case=True
@@ -176,7 +170,7 @@ validation_dataset = TensorDataset(
     encoded_batch_dev['attention_mask'],
     notes_dev)
  
-batch_size = 512
+batch_size = 128
  
 # On cree les DataLoaders d'entrainement et de validation
 # Le dataloader est juste un objet iterable
@@ -196,7 +190,7 @@ Chargement du modèle
 """
 # On prend la version pre-entrainee de DistilcamemBERT sur les sentiments
 model = CamembertForSequenceClassification.from_pretrained(text).to(device)
-#print("model 1 = ", model)
+print("model = ", model)
 # Ajouter une nouvelle couche pour 10 classes (remplacement de la dernière couche
 #new_classifier_layer = nn.Linear(model.config.hidden_size, 10).to(device)
 
@@ -364,127 +358,9 @@ for epoch in range(0, epochs):
         break
 
 print("ts = ", training_stats)
-"""
-# Pour enregistrer les stats a chaque epoque
-training_stats = []
-
-# Boucle d'entrainement
-for epoch in range(0, epochs):
-     
-    print("")
-    print(f'########## Epoch {epoch+1} / {epochs} ##########')
-    print('Training...')
- 
- 
-    # On initialise la loss pour cette epoque
-    total_train_loss = 0
- 
-    # On met le modele en mode 'training'
-    # Dans ce mode certaines couches du modele agissent differement
-    model.train()
- 
-    # Pour chaque batch
-    for step, batch in enumerate(train_dataloader):
- 
-        # On fait un print chaque 40 batchs
-        if step % 40 == 0 and not step == 0:
-            print(f'  Batch {step}  of {len(train_dataloader)}.')
-         
-        # On recupere les donnees du batch
-        input_id = batch[0].to(device)
-        attention_mask = batch[1].to(device)
-        sentiment = batch[2].to(device)
-
-        # Afficher la taille des tenseurs dans chaque batch
-        print(f"Batch {step + 1} - Taille des tenseurs:")
-        #print(f"  Input ID: {input_id.size()}")
-        #print(f"  Attention Mask: {attention_mask.size()}")
-        #print(f"  Sentiment: {sentiment.size()}")
-        
-        # On met le gradient a 0
-        optimizer.zero_grad()        
-
-        # On passe la donnee au model et on recupere la loss et le logits (sortie avant fonction d'activation)
-        
-        logits = model(input_id, token_type_ids=None, attention_mask=attention_mask).logits
-        #print("logits = ", logits)
-        # Appliquer la fonction softmax pour obtenir les probabilités
-        probabilities = F.softmax(logits, dim=1)
-        # Trouver la classe prédite pour chaque exemple dans le batch
-        output = torch.argmax(probabilities, dim=1)
-        #logits = torch.argmax(logits, dim=1).float().requires_grad_()
-        print("output = ", output, " sentiment = ", sentiment)
-        loss = criterion(output, sentiment)
-        # On incremente la loss totale
-        # .item() donne la valeur numerique de la loss
-        total_train_loss += loss.item()
- 
-        # Backpropagtion
-        loss.backward()
- 
-        # On actualise les parametrer grace a l'optimizer
-        optimizer.step()
- 
-    # On calcule la  loss moyenne sur toute l'epoque
-    avg_train_loss = total_train_loss / len(train_dataloader)   
- 
-    print("")
-    print("  Average training loss: {0:.2f}".format(avg_train_loss))  
-    
-    # Boucle de validation
-    print('Validation')
-    model.eval()  # désactive le dropout
-    total_val_loss = 0
-
-    for batch in val_dataloader:
-        # Récupération des données du batch
-        input_id = batch[0].to(device)
-        attention_mask = batch[1].to(device)
-        sentiment = batch[2].to(device)
-
-        with torch.no_grad():  # Désactive le calcul du gradient pendant la validation
-            # Récupération de la loss et des logits
-            # On passe la donnee au model et on recupere la loss et le logits (sortie avant fonction d'activation)
-            logits = model(input_id, token_type_ids=None, attention_mask=attention_mask).logits
-            #print("logits = ", logits)
-            # Appliquez la fonction argmax pour obtenir l'indice de la classe avec la probabilité la plus élevée
-            logits = torch.argmax(logits, dim=1).float()
-
-            loss = criterion(logits, sentiment)
-
-        total_val_loss += loss.item()
-
-    avg_val_loss = total_val_loss / len(val_dataloader)
-    print("  Average validation loss: {0:.2f}".format(avg_val_loss))
-
-    # Enregistrement des stats de l'epoque
-    training_stats.append(
-        {
-            'epoch': epoch + 1,
-            'Training Loss': avg_train_loss,
-            'val Loss': avg_val_loss
-        }
-    )
-
-    # Vérification de l'amélioration
-    if epoch == 0 or avg_val_loss < best_val_loss:
-        # Si la perte de validation s'améliore, enregistrez le modèle et réinitialisez le compteur
-        best_val_loss = avg_val_loss
-        epochs_without_improvement = 0
-        torch.save(model.state_dict(), "./sentiments_test.pt")
-    else:
-        # Sinon, incrémentez le compteur
-        epochs_without_improvement += 1
-
-    # Vérification de l'arrêt anticipé
-    if epochs_without_improvement >= early_stopping_patience:
-        print(f"Arrêt anticipé après {epoch+1} époques sans amélioration.")
-        break
-"""
 
 print("Model saved!")
 torch.save(model.state_dict(), "./sentiments_test.pt")
-
 
 
 
